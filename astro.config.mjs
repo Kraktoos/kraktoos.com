e070b334749c7515da429ca630724f9753f36f97
Kraktoos <kraktoos@gmail.com>
2023-04-28 19:55:37 +0100

    Remove tailwind astro plugin and add it manually

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -9,7 +8,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kraktoos.com',
-  integrations: [mdx(), sitemap(), image(), tailwind()],
+  integrations: [mdx(), sitemap(), image()],
   markdown: {
     syntaxHighlight: 'prism'
   }
