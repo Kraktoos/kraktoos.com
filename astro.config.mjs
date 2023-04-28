76d29646ae43e7cfc278212e3a0bed217459d1c0
Kraktoos <kraktoos@gmail.com>
2023-04-28 20:33:22 +0100

    Add support for svelte components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kraktoos.com',
-  integrations: [mdx(), sitemap(), image()],
+  integrations: [mdx(), sitemap(), image(), svelte()],
   markdown: {
     syntaxHighlight: 'prism'
   }
