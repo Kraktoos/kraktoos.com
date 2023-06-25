b7c8ffce825782cceea8108fa8f105ed9fb935ec
Kraktoos <kraktoos@gmail.com>
2023-06-25 21:58:09 +0100

    Add compression packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import image from "@astrojs/image";
-
 import svelte from "@astrojs/svelte";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kraktoos.com',
-  integrations: [mdx(), sitemap(), image(), svelte()],
+  integrations: [mdx(), sitemap(), image(), svelte(), purgecss(), compress()],
   markdown: {
     syntaxHighlight: 'prism'
   }
