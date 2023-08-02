7168c94ad1528d38c789bda045a2f50779c82cae
Kraktoos <kraktoos@gmail.com>
2023-08-02 10:47:10 +0100

    Optimized website builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
-
 import purgecss from "astro-purgecss";
 
-// https://astro.build/config
 import compress from "astro-compress";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://kraktoos.com',
+  site: "https://kraktoos.com",
   integrations: [mdx(), sitemap(), image(), svelte(), purgecss(), compress()],
   markdown: {
-    syntaxHighlight: 'prism'
-  }
+    syntaxHighlight: "prism",
+  },
 });
