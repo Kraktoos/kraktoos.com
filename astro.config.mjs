15cbd2a8afe8e72644e85d3cddef11e206266eb8
Kraktoos <kraktoos@gmail.com>
2023-09-01 12:10:38 +0100

    Migrate to Astro V3 and add pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
 import purgecss from "astro-purgecss";
 import compress from "astro-compress";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://kraktoos.com",
-  integrations: [mdx(), sitemap(), image(), svelte(), purgecss(), compress()],
+  integrations: [mdx(), sitemap(), svelte(), purgecss(), compress()],
   markdown: {
     syntaxHighlight: "prism",
   },
